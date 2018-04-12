dfc4003c29cefb96b4b735822c15b17ca940251e
Yuriy Homyakov <yuriy.homyakov@gmail.com>
2018-04-12 22:32:18 +0300

    scrapy fix cfg

--- jsearch/esparser/spiders/contracts_fresh.py
+++ jsearch/esparser/spiders/contracts_fresh.py
@@ -5,5 +5,7 @@ from jsearch.esparser.spiders.contracts import ContractsSpider
 
 class FreshContractsSpider(ContractsSpider):
 
+	name = 'contracts_fresh'
+
     def get_next_page_request(self, response):
         return scrapy.Request(url=self.listing_url, dont_filter=True, callback=self.parse_listing)
