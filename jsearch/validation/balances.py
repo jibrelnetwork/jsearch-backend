fa3439d49eecd66a6fcfc6e05c878c63f1a94e0f
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-01-14 18:07:18 +0300

    Fix

--- jsearch/validation/balances.py
+++ jsearch/validation/balances.py
@@ -43,7 +43,7 @@ async def update_token_holder_balance(pool: Pool, token_address: str,
     query = f"""
         UPDATE token_holders 
         SET balance = $3, decimals = $4
-        WHERE accounts_address = $1 and token_address = $2;
+        WHERE account_address = $1 and token_address = $2;
     """
     async with pool.acquire() as conn:
             await conn.execute(query, account_address, token_address, balance, decimals)
