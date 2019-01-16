b6f70924940348c01e81010bf852321f5eaa55f1
Yuriy Homyakov <yuriy.homyakov@gmail.com>
2019-01-16 15:42:54 +0400

    fix no reward issue, close raw_db connection asap

--- jsearch/common/migrations/versions/d44ee9b43f8d_add_token_transfers_table.py
+++ jsearch/common/migrations/versions/d44ee9b43f8d_add_token_transfers_table.py
@@ -12,7 +12,7 @@ from jsearch.common import tables
 
 # revision identifiers, used by Alembic.
 revision = 'd44ee9b43f8d'
-down_revision = '663cb3c4d3ba'
+down_revision = '2b3b48c8762b'
 branch_labels = None
 depends_on = None
 
@@ -23,14 +23,27 @@ UP_SQL = """CREATE TABLE token_transfers (
                 transaction_hash varchar,
                 log_index integer,
                 block_number integer,
+                block_hash integer,
                 timestamp integer,
                 from_address varchar,
                 to_address varchar,
+                token_address varchar,
                 token_value numeric,
+                token_decimals integer,
                 token_name varchar,
                 token_symbol varchar,
-                );"""
-DOWN_SQL = """ALTER TABLE reorgs DROP COLUMN node_id"""
+                is_forked bool default false
+                );
+                
+            CREATE INDEX ix_token_transfers_address_block_number_log_index ON token_transfers(address, block_number, log_index);
+            CREATE INDEX ix_token_transfers_token_address_block_number_log_index ON token_transfers(token_address, block_number, log_index);
+            CREATE INDEX ix_token_transfers_block_hash ON token_transfers(block_hash);
+            """
+DOWN_SQL = """
+            DROP TABLE token_transfers;
+            DROP INDEX ix_token_transfers_address_block_number_log_index;
+            DROP INDEX ix_token_transfers_block_hash;
+            """
 
 
 def upgrade():
