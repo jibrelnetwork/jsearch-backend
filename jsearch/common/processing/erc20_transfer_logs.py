605639dbfd7b5f9447132f6fb429fb238abc2158
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-01-18 18:15:49 +0300

    Event contract does not found - create transfer record

--- jsearch/common/processing/erc20_transfer_logs.py
+++ jsearch/common/processing/erc20_transfer_logs.py
@@ -187,9 +187,9 @@ def log_to_transfers(log: Log, block: Block, contract: Contract) -> Transfers:
         'timestamp': block['timestamp'],
         'to_address': log['token_transfer_to'],
         'token_address': log['address'],
-        'token_decimals': contract['decimals'],
-        'token_name': contract['token_name'],
-        'token_symbol': contract['token_symbol'],
+        'token_decimals': contract and contract['decimals'],
+        'token_name': contract and contract['token_name'],
+        'token_symbol': contract and contract['token_symbol'],
         'token_value': log['token_amount'],
         'transaction_hash': log['transaction_hash']
     }
@@ -204,6 +204,6 @@ def logs_to_transfers(logs: Logs, blocks: Dict[str, Block], contracts: Dict[str,
     for log in logs:
         contract = contracts.get(log['address'])
         block = blocks.get(log['block_hash'])
-        if block and log and contract and log.get('is_token_transfer'):
+        if log.get('is_token_transfer'):
             transfers.extend(log_to_transfers(log, block, contract))
     return transfers
