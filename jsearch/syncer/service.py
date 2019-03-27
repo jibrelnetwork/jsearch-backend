2a44589f9d7078c05d8500cf8c729ebd536d48d3
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-03-27 16:11:02 +0300

    Use function `parse_range` instead plain code.

--- jsearch/syncer/service.py
+++ jsearch/syncer/service.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
 
-from jsearch.syncer.database import MainDB, RawDB
 from jsearch import settings
+from jsearch.syncer.database import MainDB, RawDB
+from jsearch.utils import parse_range
 from .manager import Manager
-
 
 logger = logging.getLogger(__name__)
 
@@ -13,17 +13,13 @@ class Service:
     """
     Component container
     """
+
     def __init__(self, options):
         self.options = options
         self.raw_db = RawDB(settings.JSEARCH_RAW_DB)
         self.main_db = MainDB(settings.JSEARCH_MAIN_DB)
-        sync_range = [0, None]
-        if options.sync_range:
-            parts = [p.strip() for p in options.sync_range.split('-')]
-            if len(parts) != 2:
-                raise ValueError('Invalid sync_range option')
-            sync_range[0] = int(parts[0]) if parts[0] else 1
-            sync_range[1] = int(parts[1]) if parts[1] else None
+
+        sync_range = parse_range(value=options.sync_range)
         self.manager = Manager(self, self.main_db, self.raw_db, sync_range=sync_range)
 
     def run(self):
