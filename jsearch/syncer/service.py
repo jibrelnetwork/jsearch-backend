dee44e31f6dcc04e1738982a0aaabe0b6dd0263a
ridhid <asiforis@gmail.com>
2019-03-27 19:45:24 +0300

    Gracefully shutdown for syncer. (#158)
    * Use function `parse_range` instead plain code.
    
    * Add gracefully shutdown for syncer.
    
    * Change string values to constants
    
    * Fix codestyle issues:

--- jsearch/syncer/service.py
+++ jsearch/syncer/service.py
@@ -1,10 +1,11 @@
 import asyncio
 import logging
+import signal
 
-from jsearch.syncer.database import MainDB, RawDB
 from jsearch import settings
+from jsearch.syncer.database import MainDB, RawDB
+from jsearch.utils import shutdown
 from .manager import Manager
-
 
 logger = logging.getLogger(__name__)
 
@@ -13,38 +14,51 @@ class Service:
     """
     Component container
     """
-    def __init__(self, options):
-        self.options = options
+    _is_need_to_stop: bool = False
+
+    def __init__(self, sync_range):
         self.raw_db = RawDB(settings.JSEARCH_RAW_DB)
         self.main_db = MainDB(settings.JSEARCH_MAIN_DB)
-        sync_range = [0, None]
-        if options.sync_range:
-            parts = [p.strip() for p in options.sync_range.split('-')]
-            if len(parts) != 2:
-                raise ValueError('Invalid sync_range option')
-            sync_range[0] = int(parts[0]) if parts[0] else 1
-            sync_range[1] = int(parts[1]) if parts[1] else None
         self.manager = Manager(self, self.main_db, self.raw_db, sync_range=sync_range)
 
-    def run(self):
+    async def run(self):
         """
         Start all process
         """
 
         logger.info("Starting jSearch Syncer")
 
-        loop = asyncio.get_event_loop()
-
-        loop.run_until_complete(self.raw_db.connect())
-        loop.run_until_complete(self.main_db.connect())
+        await self.raw_db.connect()
+        await self.main_db.connect()
 
         asyncio.ensure_future(self.manager.run())
+        asyncio.ensure_future(self.monitor())
 
         logger.info("Up and running!")
 
-    def stop(self):
+    async def stop(self):
         logger.info("Stopping jSearch Syncer")
-        self.manager.stop()
-        self.main_db.disconnect()
+        await self.manager.stop()
+        await self.main_db.disconnect()
         self.raw_db.disconnect()
         logger.info("Bye!")
+
+    def gracefully_shutdown(self):
+        self._is_need_to_stop = True
+
+        loop = asyncio.get_event_loop()
+        loop.remove_signal_handler(sig=signal.SIGTERM)
+        loop.remove_signal_handler(sig=signal.SIGINT)
+
+    async def monitor(self):
+        while not self._is_need_to_stop:
+            await asyncio.sleep(0.5)
+
+        logging.info(f'Received exit signal ...')
+        logging.info('Stop service bus...')
+
+        await self.stop()
+
+        asyncio.ensure_future(shutdown())
+
+        logging.info('Shutdown complete.')
