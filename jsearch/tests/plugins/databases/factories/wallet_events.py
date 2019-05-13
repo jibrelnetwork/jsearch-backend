dfd90da687538b4d5d8c233cb4b932b563c3e214
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-05-13 18:34:18 +0300

    JSEARCH-243
    Add missed factories for tests.

---
+++ jsearch/tests/plugins/databases/factories/wallet_events.py
@@ -0,0 +1,62 @@
+from random import randint
+
+import factory
+import pytest
+
+from jsearch.common.tables import wallet_events_t
+from jsearch.tests.plugins.databases.factories.common import generate_address
+from .common import session, Base
+
+
+class WalletEventsModel(Base):
+    __table__ = wallet_events_t
+    __mapper_args__ = {
+        'primary_key': [
+            wallet_events_t.c.address,
+            wallet_events_t.c.tx_hash,
+            wallet_events_t.c.block_hash,
+            wallet_events_t.c.block_number,
+            wallet_events_t.c.event_index
+        ]
+    }
+
+
+class WalletEventsFactory(factory.alchemy.SQLAlchemyModelFactory):
+    address = factory.LazyFunction(generate_address)
+    block_number = factory.Sequence(lambda n: n)
+    block_hash = factory.LazyFunction(generate_address)
+    tx_hash = factory.LazyFunction(generate_address)
+    event_index = factory.Sequence(lambda n: n % 10)
+    event_data = factory.LazyFunction(dict)
+    type = 'erc20-transfer'
+    is_forked = False
+
+    class Meta:
+        model = WalletEventsModel
+        sqlalchemy_session = session
+        sqlalchemy_session_persistence = 'flush'
+
+    @classmethod
+    def create_token_transfer(cls, block=None, **kwargs):
+        to = generate_address()
+        from_ = generate_address()
+        amount = randint(0, 10 * 18)
+
+        defaults = dict(
+            address=from_,
+            type='erc20-transfer',
+            event_data={'sender': to, 'recipient': from_, 'amount': amount},
+        )
+        defaults.update(**kwargs)
+        if block:
+            defaults.update(
+                block_hash=block.hash,
+                block_number=block.number
+            )
+
+        return cls.create(**defaults)
+
+
+@pytest.fixture()
+def wallet_events_factory():
+    return WalletEventsFactory
