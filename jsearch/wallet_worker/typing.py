cbf482bbd456b7fca81d85f64c8f7dbc1f7d17a8
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-05-14 18:43:20 +0300

    JSEARCH-243
    Separate wallet worker to few small pieces.

---
+++ jsearch/wallet_worker/typing.py
@@ -0,0 +1,8 @@
+from typing import Dict, Any
+
+Event = Dict[str, Any]
+Transaction = Dict[str, Any]
+InternalTransaction = Dict[str, Any]
+TokenTransfer = Dict[str, Any]
+AssetTransfer = Dict[str, Any]
+AssetUpdate = Dict[str, Any]
