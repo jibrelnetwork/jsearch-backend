d17d8fc5b4ce5dcb625c9979a9e3fb9fd294edee
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-05-17 14:46:50 +0300

    JSEARCH-234 \n  Refactoring unit tests for wallet worker

--- jsearch/wallet_worker/tests/test_database_service.py
+++ jsearch/wallet_worker/tests/test_database_service.py
@@ -15,167 +15,192 @@ pytest_plugins = [
 ]
 
 
-@pytest.mark.usefixtures('mock_service_bus')
-async def test_add_or_update_asset_summary_balance(db):
-    s = DatabaseService()
-    await s.on_start()
+@pytest.fixture
+async def database_service(db, loop, mock_service_bus):
+    service = DatabaseService()
+    await service.on_start()
+    yield service
+    await service.on_stop()
 
+
+async def test_add_or_update_asset_summary_balance(db, database_service):
+    # given
     asset_update = {
         'asset_address': '0xc1',
         'address': '0xa1',
         'value': 100,
         'decimals': 10,
     }
-    await s.add_or_update_asset_summary_balance(asset_update)
+    await database_service.add_or_update_asset_summary_balance(asset_update)
 
     res = db.execute(assets_summary_t.select()).fetchall()
     assert len(res) == 1
-    assert dict(res[0]) == {'address': asset_update['address'],
-                            'asset_address': asset_update['asset_address'],
-                            'value': asset_update['value'],
-                            'decimals': asset_update['decimals'],
-                            'tx_number': 1,
-                            'nonce': None
-                            }
-
-    asset_update = {
-        'asset_address': '0xc1',
-        'address': '0xa1',
-        'value': 100,
-        'decimals': 10,
+    assert dict(res[0]) == {
+        'address': asset_update['address'],
+        'asset_address': asset_update['asset_address'],
+        'value': asset_update['value'],
+        'decimals': asset_update['decimals'],
+        'tx_number': 1,
+        'nonce': None
     }
-    await s.add_or_update_asset_summary_balance(asset_update)
 
     res = db.execute(assets_summary_t.select()).fetchall()
     assert len(res) == 1
-    assert dict(res[0]) == {'address': asset_update['address'],
-                            'asset_address': asset_update['asset_address'],
-                            'value': asset_update['value'],
-                            'decimals': asset_update['decimals'],
-                            'tx_number': 1,
-                            'nonce': None
-                            }
+    assert dict(res[0]) == {
+        'address': asset_update['address'],
+        'asset_address': asset_update['asset_address'],
+        'value': asset_update['value'],
+        'decimals': asset_update['decimals'],
+        'tx_number': 1,
+        'nonce': None
+    }
 
 
 @pytest.mark.usefixtures('mock_service_bus')
-async def test_add_or_update_asset_summary_transfer(db):
-    s = DatabaseService()
-    await s.on_start()
-
+async def test_add_or_update_asset_summary_transfer(db, database_service):
     asset_transfer = {
         'token_address': '0xc2',
         'address': '0xa2',
     }
-    await s.add_or_update_asset_summary_from_transfer(asset_transfer)
+    await database_service.add_or_update_asset_summary_from_transfer(asset_transfer)
 
     res = db.execute(assets_summary_t.select()).fetchall()
     assert len(res) == 1
-    assert dict(res[0]) == {'address': asset_transfer['address'],
-                            'asset_address': asset_transfer['token_address'],
-                            'value': None,
-                            'decimals': None,
-                            'tx_number': 1,
-                            'nonce': None
-                            }
+    assert dict(res[0]) == {
+        'address': asset_transfer['address'],
+        'asset_address': asset_transfer['token_address'],
+        'value': None,
+        'decimals': None,
+        'tx_number': 1,
+        'nonce': None
+    }
 
-    await s.add_or_update_asset_summary_from_transfer(asset_transfer)
+    await database_service.add_or_update_asset_summary_from_transfer(asset_transfer)
 
     res = db.execute(assets_summary_t.select()).fetchall()
     assert len(res) == 1
-    assert dict(res[0]) == {'address': asset_transfer['address'],
-                            'asset_address': asset_transfer['token_address'],
-                            'value': None,
-                            'decimals': None,
-                            'tx_number': 2,
-                            'nonce': None
-                            }
+    assert dict(res[0]) == {
+        'address': asset_transfer['address'],
+        'asset_address': asset_transfer['token_address'],
+        'value': None,
+        'decimals': None,
+        'tx_number': 2,
+        'nonce': None
+    }
 
 
-async def test_add_wallet_event_token_transfer(db, transfer_factory, transaction_factory):
-    s = DatabaseService()
-    await s.on_start()
+async def test_add_wallet_event_token_transfer(db, transfer_factory, transaction_factory, database_service):
+    # given
     tx = transaction_factory.create()
     tx_data = tx.to_dict()
     del tx_data['address']
     tx_data['receipt_status'] = 1
-    transfer_data = factory.build(dict, FACTORY_CLASS=transfer_factory,
-                                  transaction_hash=tx.hash, token_value=5 * 10 ** 18, token_decimals=18)
+    transfer_data = factory.build(
+        dict,
+        FACTORY_CLASS=transfer_factory,
+        transaction_hash=tx.hash,
+        token_value=5 * 10 ** 18,
+        token_decimals=18
+    )
     transfer_data['status'] = 1
-    await s.add_wallet_event_token_transfer(transfer_data)
 
+    # when
+    await database_service.add_wallet_event_token_transfer(transfer_data)
+
+    # then
     res = db.execute(wallet_events_t.select()).fetchall()
     del tx_data['receipt_status']
     assert len(res) == 2
-    assert dict(res[0]) == {'address': transfer_data['from_address'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': transfer_data['to_address'],
-                                           'sender': transfer_data['from_address'],
-                                           'amount': '5.0',
-                                           'asset': transfer_data['token_address'],
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'erc20-transfer'}
-    assert dict(res[1]) == {'address': transfer_data['to_address'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': transfer_data['to_address'],
-                                           'sender': transfer_data['from_address'],
-                                           'amount': '5.0',
-                                           'asset': transfer_data['token_address'],
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'erc20-transfer'}
+
+    first, second = map(dict, res)
+    assert first == {
+        'address': transfer_data['from_address'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': transfer_data['to_address'],
+            'sender': transfer_data['from_address'],
+            'amount': '5.0',
+            'asset': transfer_data['token_address'],
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'erc20-transfer'
+    }
+    assert second == {
+        'address': transfer_data['to_address'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': transfer_data['to_address'],
+            'sender': transfer_data['from_address'],
+            'amount': '5.0',
+            'asset': transfer_data['token_address'],
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'erc20-transfer'
+    }
 
 
-async def test_add_wallet_event_tx(db, transaction_factory):
-    s = DatabaseService()
-    await s.on_start()
+async def test_add_wallet_event_tx(db, transaction_factory, database_service):
+    # given
     tx = transaction_factory.create(input='0x', value='0xffff')
     tx_data = tx.to_dict()
     del tx_data['address']
     tx_data['to_contract'] = False
     tx_data['receipt_status'] = 1
 
-    await s.add_wallet_event_tx(tx_data)
+    # when
+    await database_service.add_wallet_event_tx(tx_data)
 
+    # then
     res = db.execute(wallet_events_t.select()).fetchall()
     assert len(res) == 2
-    assert dict(res[0]) == {'address': tx_data['from'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': tx_data['to'],
-                                           'sender': tx_data['from'],
-                                           'amount': '65535',
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'eth-transfer'}
-    assert dict(res[1]) == {'address': tx_data['to'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': tx_data['to'],
-                                           'sender': tx_data['from'],
-                                           'amount': '65535',
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'eth-transfer'}
+
+    first, second = map(dict, res)
+    assert first == {
+        'address': tx_data['from'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': tx_data['to'],
+            'sender': tx_data['from'],
+            'amount': '65535',
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'eth-transfer'
+    }
+    assert second == {
+        'address': tx_data['to'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': tx_data['to'],
+            'sender': tx_data['from'],
+            'amount': '65535',
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'eth-transfer'
+    }
 
 
-async def test_add_wallet_event_tx_internal(db, transaction_factory):
-    s = DatabaseService()
-    await s.on_start()
+async def test_add_wallet_event_tx_internal(db, transaction_factory, database_service):
+    # given
     tx = transaction_factory.create(input='0x', value='0xffff')
     tx_data = tx.to_dict()
     del tx_data['address']
@@ -189,31 +214,43 @@ async def test_add_wallet_event_tx_internal(db, transaction_factory):
         'to': '0xbb',
     }
 
-    await s.add_wallet_event_tx_internal(tx_data, internal_tx_data)
+    # when
+    await database_service.add_wallet_event_tx_internal(tx_data, internal_tx_data)
 
+    # then
     res = db.execute(wallet_events_t.select()).fetchall()
     assert len(res) == 2
-    assert dict(res[0]) == {'address': internal_tx_data['from'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': internal_tx_data['to'],
-                                           'sender': internal_tx_data['from'],
-                                           'amount': '12000',
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'eth-transfer'}
-    assert dict(res[1]) == {'address': internal_tx_data['to'],
-                            'block_hash': tx_data['block_hash'],
-                            'block_number': tx_data['block_number'],
-                            'event_data': {'recipient': internal_tx_data['to'],
-                                           'sender': internal_tx_data['from'],
-                                           'amount': '12000',
-                                           'status': 1},
-                            'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
-                            'is_forked': False,
-                            'tx_data': tx_data,
-                            'tx_hash': tx_data['hash'],
-                            'type': 'eth-transfer'}
+
+    first, second = map(dict, res)
+    assert first == {
+        'address': internal_tx_data['from'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': internal_tx_data['to'],
+            'sender': internal_tx_data['from'],
+            'amount': '12000',
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'eth-transfer'
+    }
+    assert second == {
+        'address': internal_tx_data['to'],
+        'block_hash': tx_data['block_hash'],
+        'block_number': tx_data['block_number'],
+        'event_data': {
+            'recipient': internal_tx_data['to'],
+            'sender': internal_tx_data['from'],
+            'amount': '12000',
+            'status': 1
+        },
+        'event_index': 1000 * tx_data['block_number'] + tx_data['transaction_index'],
+        'is_forked': False,
+        'tx_data': tx_data,
+        'tx_hash': tx_data['hash'],
+        'type': 'eth-transfer'
+    }
