6553f045bac106b8b4e4664fc6d31e4f87f5336c
Nick Gashkov <nick@gashkov.com>
2019-05-31 11:51:20 +0300

    JSEARCH-336: Fix 'validation' and 'worker' entrypoints

--- jsearch/validation/__main__.py
+++ jsearch/validation/__main__.py
@@ -13,23 +13,7 @@ from jsearch.validation.proxy import TokenProxy
 logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.argument('token')
-@click.option('--check-balances', is_flag=True)
-@click.option('--show-holders', is_flag=True)
-@click.option('--rewrite', is_flag=True)
-@click.option('--log-level', default=settings.LOG_LEVEL, help="Log level")
-@click.option('--no-json-formatter', is_flag=True, default=settings.NO_JSON_FORMATTER, help='Use default formatter')
-def check(
-        token: str,
-        check_balances: bool,
-        rewrite: bool,
-        show_holders: bool,
-        log_level: str,
-        no_json_formatter: bool,
-) -> None:
-
-    logs.configure(log_level=log_level, formatter_class=logs.select_formatter_class(no_json_formatter))
+def run(token: str, check_balances: bool, rewrite: bool, show_holders: bool) -> None:
     loop = asyncio.get_event_loop()
 
     tokens = service_bus.get_contracts(addresses=[token])
@@ -48,5 +32,25 @@ def check(
         logger.info('Token was not found')
 
 
+@click.command()
+@click.argument('token')
+@click.option('--check-balances', is_flag=True)
+@click.option('--show-holders', is_flag=True)
+@click.option('--rewrite', is_flag=True)
+@click.option('--log-level', default=settings.LOG_LEVEL, help="Log level")
+@click.option('--no-json-formatter', is_flag=True, default=settings.NO_JSON_FORMATTER, help='Use default formatter')
+def main(
+        token: str,
+        check_balances: bool,
+        rewrite: bool,
+        show_holders: bool,
+        log_level: str,
+        no_json_formatter: bool,
+) -> None:
+
+    logs.configure(log_level=log_level, formatter_class=logs.select_formatter_class(no_json_formatter))
+    run(token, check_balances, rewrite, show_holders)
+
+
 if __name__ == '__main__':
-    check()
+    main()
