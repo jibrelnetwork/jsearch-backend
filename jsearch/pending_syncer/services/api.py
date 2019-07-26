18df856db8499c226763bde13d2c3aba57b9a89b
Nick Gashkov <nick@gashkov.com>
2019-07-26 17:37:28 +0300

    JSEARCH-237: Remove obsolete environment variables

--- jsearch/pending_syncer/services/api.py
+++ jsearch/pending_syncer/services/api.py
@@ -14,7 +14,7 @@ from jsearch.common import services
 
 class ApiService(services.ApiService):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        kwargs.setdefault('port', settings.WALLET_WORKER_API_PORT)
+        kwargs.setdefault('port', settings.SYNCER_PENDING_API_PORT)
         kwargs.setdefault('app_maker', make_app)
 
         super(ApiService, self).__init__(*args, **kwargs)
