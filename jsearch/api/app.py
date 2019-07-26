9b96b302c8a1963153c4ce02fbef963027579fcc
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2019-07-26 15:10:10 +0300

    JSEARCH-424
    Add proxy prefix into endpoints:
    - /v1/gas_price
    - /v1/transaction_count
    - /v1/estimate_gas
    - /v1/call_contract
    - /v1/send_raw_transaction

--- jsearch/api/app.py
+++ jsearch/api/app.py
@@ -76,11 +76,11 @@ async def make_app():
     app.router.add_route('GET', '/v1/tokens/{address}/transfers', tokens.get_token_transfers)
     app.router.add_route('GET', '/v1/tokens/{address}/holders', tokens.get_token_holders)
 
-    app.router.add_route('GET', '/v1/gas_price', node_proxy.get_gas_price)
-    app.router.add_route('POST', '/v1/transaction_count', node_proxy.get_transaction_count)
-    app.router.add_route('POST', '/v1/estimate_gas', node_proxy.calculate_estimate_gas)
-    app.router.add_route('POST', '/v1/call_contract', node_proxy.call_contract)
-    app.router.add_route('POST', '/v1/send_raw_transaction', node_proxy.send_raw_transaction)
+    app.router.add_route('GET', '/v1/proxy/gas_price', node_proxy.get_gas_price)
+    app.router.add_route('POST', '/v1/proxy/transaction_count', node_proxy.get_transaction_count)
+    app.router.add_route('POST', '/v1/proxy/estimate_gas', node_proxy.calculate_estimate_gas)
+    app.router.add_route('POST', '/v1/proxy/call_contract', node_proxy.call_contract)
+    app.router.add_route('POST', '/v1/proxy/send_raw_transaction', node_proxy.send_raw_transaction)
 
     app.router.add_route('GET', '/v1/wallet/blockchain_tip', wallets.get_blockchain_tip)
     app.router.add_route('GET', '/v1/wallet/assets_summary', wallets.get_assets_summary)
