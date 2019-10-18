f60c1160b009a27a891db327f07d7d836919eb45
Nick Gashkov <nick@gashkov.com>
2019-10-18 13:54:49 +0300

    ETHBE-674: Add 'BlockFactory.create_with_event' and tests for empty data

--- jsearch/api/helpers.py
+++ jsearch/api/helpers.py
@@ -7,6 +7,7 @@ from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Query
 from typing import Any, Dict, List, Optional, Union, Callable, TypeVar
 
+from jsearch import settings
 from jsearch.api.error_code import ErrorCode
 from jsearch.api.pagination import Page
 from jsearch.common.utils import async_timeit
@@ -278,3 +279,24 @@ async def load_json_or_raise_api_error(request: web.Request) -> Json:
             },
             status=400,
         )
+
+
+async def maybe_orphan_request(
+        request: web.Request,
+        last_chain_insert_id: Optional[int],
+        last_data_block: Optional[int],
+        last_tip_block: Optional[int],
+) -> Optional[web.Response]:
+    """Orphans request if data consistency was affected by a chain split."""
+    storage = request.app['storage']
+    requests_orphaned_metric = request.app['metrics']['REQUESTS_ORPHANED']
+
+    last_block = max((last_data_block, last_tip_block), key=lambda x: x or 0)
+    should_orphan = await storage.is_data_affected_by_chain_split(last_chain_insert_id, last_block)
+
+    if should_orphan:
+        requests_orphaned_metric.labels(settings.PID, request.path).inc()
+
+        return api_success(data={"isOrphaned": True})
+
+    return None
