dd77b419ee1f9aa066c6e5987a0976c29a7a1940
Nick Gashkov <nick@gashkov.com>
2019-10-22 17:07:26 +0300

    ETHBE-710: Add 'event_index'/'block_number' validation

--- jsearch/api/serializers/wallets.py
+++ jsearch/api/serializers/wallets.py
@@ -1,6 +1,6 @@
 import logging
 
-from marshmallow import fields
+from marshmallow import fields, validates_schema, ValidationError
 from marshmallow.validate import Range, Length
 
 from jsearch.api.database_queries.wallet_events import get_events_ordering
@@ -8,6 +8,7 @@ from jsearch.api.helpers import Tag
 from jsearch.api.ordering import Ordering
 from jsearch.api.serializers.common import BlockRelatedListSchema
 from jsearch.api.serializers.fields import PositiveIntOrTagField, StrLower, IntField, BigIntField
+from jsearch.common.wallet_events import is_event_belongs_to_block, make_event_index
 from jsearch.typing import OrderScheme, OrderDirection
 
 logger = logging.getLogger(__name__)
@@ -36,3 +37,20 @@ class WalletEventsSchema(BlockRelatedListSchema):
 
     def _get_ordering(self, scheme: OrderScheme, direction: OrderDirection) -> Ordering:
         return get_events_ordering(scheme, direction)
+
+    @validates_schema
+    def validate_event_index_block_number_relation(self, data, **kwargs):
+        event_index = data.get("event_index")
+        block_number = data.get("block_number")
+
+        if event_index is None or block_number is None or is_event_belongs_to_block(event_index, block_number):
+            return
+
+        lower_bound = make_event_index(block_number, 0, 0)
+        upper_bound = make_event_index(block_number, 999, 9999)
+
+        raise ValidationError(
+            f"Must belong to block {block_number} and be between {lower_bound} "
+            f"and {upper_bound}.",
+            ["event_index"],
+        )
