9c4435a91de77e9dc9b255f89f43883c758d8882
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-10-22 14:45:43 +0300

    temp

--- migrations/20191022111522_test.sql
+++ migrations/20191022111522_test.sql
@@ -12,11 +12,19 @@ BEGIN
 END;
 $$;
 
-CREATE FUNCTION insert_block_data(block_data text, uncles_data text, transactions_data text, receipts_data text,
-                                  logs_data text, accounts_state_data text, accounts_base_data text,
-                                  internal_txs_data text, transfers_data text, token_holders_updates_data text,
-                                  wallet_events_data text, assets_summary_updates_data text,
-                                  chain_event_data text) RETURNS boolean
+CREATE OR REPLACE FUNCTION insert_block_data(block_data text,
+                                             uncles_data text,
+                                             transactions_data text,
+                                             receipts_data text,
+                                             logs_data text,
+                                             accounts_state_data text,
+                                             accounts_base_data text,
+                                             internal_txs_data text,
+                                             transfers_data text,
+                                             token_holders_updates_data text,
+                                             wallet_events_data text,
+                                             assets_summary_updates_data text,
+                                             chain_event_data text) RETURNS boolean
     LANGUAGE plpgsql
 AS
 $$
