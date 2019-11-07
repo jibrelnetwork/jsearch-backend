5979bbde840128eeae4b70cc3c86fb0eb355fc44
Nick Gashkov <nick@gashkov.com>
2019-11-07 17:43:05 +0300

    ETHBE-708: Add new scheme waiter for Blocks Syncer

--- jsearch/syncer/main.py
+++ jsearch/syncer/main.py
@@ -1,7 +1,9 @@
+import asyncio
 import logging
 
 import click
 import time
+from aiopg.sa import create_engine
 from typing import Any, Dict
 
 from jsearch import settings
@@ -27,7 +29,7 @@ def run_worker(sync_range: BlockRange, api_port: int, check_lag: bool, check_hol
     worker.Worker(syncer).execute_from_commandline()
 
 
-def run_workers_pool(sync_range: BlockRange, workers: int, **kwargs: Dict[str, Any]):
+def run_workers_pool(sync_range: BlockRange, workers: int, **kwargs: Dict[str, Any]) -> None:
     pool = WorkersPool(
         sync_range=sync_range,
         workers=workers,
@@ -39,6 +41,39 @@ def run_workers_pool(sync_range: BlockRange, workers: int, **kwargs: Dict[str, A
 
     api_service.add_dependency(pool_service)
     worker.Worker(api_service).execute_from_commandline()
+
+
+# TODO (nickgashkov): Remove after schema this migration is applied:
+#   20191107070950_add_assets_summary_pairs.sql
+async def wait_new_scheme() -> None:
+    query = """
+    SELECT *
+    FROM information_schema.tables
+    WHERE table_name = 'assets_summary_pairs';
+    """
+
+    engine = await create_engine(dsn=settings.JSEARCH_MAIN_DB, maxsize=1)
+
+    while True:
+        try:
+            async with engine.acquire() as connection:
+                async with connection.execute(query) as cursor:
+                    row = await cursor.fetchone()
+                    if row is None:
+                        logger.info('Wait new scheme, wait 5 seconds...')
+                        await asyncio.sleep(5)
+                    else:
+                        logger.info('New schema have founded, start sync...')
+                        break
+        except KeyboardInterrupt:
+            break
+
+    engine.close()
+
+
+def wait() -> None:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(wait_new_scheme())
 
 
 @click.command()
@@ -59,15 +94,18 @@ def run(
         check_lag: bool,
         check_holes: bool,
         no_json_formatter: bool,
-):
+) -> None:
     stats.setup_syncer_metrics()
     logs.configure(log_level=log_level, formatter_class=logs.select_formatter_class(no_json_formatter))
 
-    sync_range = BlockRange(*parse_range(sync_range))
+    wait()
+
+    block_range = BlockRange(*parse_range(sync_range))
+
     if workers > 1:
         logger.info("Scale... ", extra={"workers": workers})
         run_workers_pool(
-            sync_range=sync_range,
+            sync_range=block_range,
             workers=workers,
             **{
                 'check_lag': 0,
@@ -78,7 +116,7 @@ def run(
             }
         )
     else:
-        run_worker(sync_range, port, check_lag, check_holes, resync)
+        run_worker(block_range, port, check_lag, check_holes, resync)
 
 
 if __name__ == '__main__':
