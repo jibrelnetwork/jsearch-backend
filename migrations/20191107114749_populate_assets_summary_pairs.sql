70862abf881e475e87365a693bde45951e159112
Nick Gashkov <nick@gashkov.com>
2019-11-07 17:13:04 +0300

    ETHBE-708: Reorder migrations to migrate scheme before data

---
+++ migrations/20191107114749_populate_assets_summary_pairs.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+-- At this moment, Blocks Syncer could already sync some pairs into the table,
+-- so use 'UPSERT' here.
+INSERT INTO assets_summary_pairs
+SELECT DISTINCT address, asset_address
+FROM assets_summary
+WHERE asset_address != ''
+ON CONFLICT DO NOTHING;
+
+CREATE UNIQUE INDEX ix_assets_summary_pairs_address_asset_address ON assets_summary_pairs USING btree (address, asset_address);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX ix_assets_summary_pairs_address_asset_address;
+-- +goose StatementEnd
