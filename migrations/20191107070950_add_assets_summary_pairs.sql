405ea2e21b9aab469c19eb4463e5363efe755b4a
Nick Gashkov <nick@gashkov.com>
2019-11-07 14:01:59 +0300

    ETHBE-708: Update 'assets_summary_pairs' data migration
    This is made to avoid 'UNIQUE' constraint violation if Blocks Syncer
    have already written pairs to the table before this migration is
    applied.
    
    Also, ETH assets have been removed from pairs due to its obsolence.
    Basically, every EOA has some ether.

--- migrations/20191107070950_add_assets_summary_pairs.sql
+++ migrations/20191107070950_add_assets_summary_pairs.sql
@@ -6,9 +6,13 @@ CREATE TABLE assets_summary_pairs
     asset_address character varying NOT NULL
 );
 
+-- At this moment, Blocks Syncer could already sync some pairs into the table,
+-- so use 'UPSERT' here.
 INSERT INTO assets_summary_pairs
 SELECT DISTINCT address, asset_address
-FROM assets_summary;
+FROM assets_summary
+WHERE asset_address != ''
+ON CONFLICT DO NOTHING;
 
 CREATE UNIQUE INDEX ix_assets_summary_pairs_address_asset_address ON assets_summary_pairs USING btree (address, asset_address);
 -- +goose StatementEnd
