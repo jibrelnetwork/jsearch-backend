5b5072e0b8997af9af71a5af0cb6376bc86b0e5d
Nick Gashkov <nick@gashkov.com>
2019-11-20 22:30:33 +0300

    wip: query-bencher

---
+++ bencher.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import asyncio
+import os
+
+import sys
+
+import time
+import statistics
+import click
+import yaml
+from typing import Any, Dict, List, NamedTuple
+
+from aiopg import sa
+
+
+class QueryWithParams(NamedTuple):
+    query: str
+    params: Dict[str, Any]
+
+
+async def bench_once(db: sa.Engine, query: QueryWithParams) -> int:
+    started_at = time.perf_counter_ns()
+
+    async with db.acquire() as conn:
+        await conn.execute(query.query, query.params)
+
+    ended_at = time.perf_counter_ns()
+
+    return ended_at - started_at
+
+
+async def bench(dsn: str, queries: List[QueryWithParams], times: int) -> None:
+    db = await sa.create_engine(dsn)
+
+    coros = []
+
+    for query in queries:
+        coros.extend(bench_once(db, query) for __ in range(times))
+
+    results: List[int] = await asyncio.gather(*coros)
+
+    sys.stdout.write('========== RESULTS ==========' + os.linesep)
+    sys.stdout.write(f'- min: {min(results) // 1000000} ms' + os.linesep)
+    sys.stdout.write(f'- max: {max(results) // 1000000} ms' + os.linesep)
+    sys.stdout.write(f'- mean: {statistics.mean(results) // 1000000} ms' + os.linesep)
+    sys.stdout.write(f'- median: {statistics.median(results) // 1000000} ms' + os.linesep)
+    sys.stdout.write(f'- total: {sum(results) // 1000000} ms' + os.linesep)
+    sys.stdout.write(f'- all: {", ".join(map(lambda r: str(r // 1000000), results))} ms' + os.linesep)
+
+    db.close()
+    await db.wait_closed()
+
+
+@click.command()
+@click.argument('dsn', type=str)  # Target DB's DSN
+@click.argument('data-filename', type=click.Path(exists=True))  # YAML with a query to benchmark and it's data
+@click.option('--times', type=int, default=50, help="Times to run a query")
+# TODO: pool sizes
+def main(dsn: str, data_filename: str, times: int) -> None:
+    with open(data_filename) as data:
+        queries_raw: List[Dict[str, Any]] = yaml.safe_load(data)
+
+    queries = [QueryWithParams(**q) for q in queries_raw]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bench(dsn, queries, times))
+
+
+if __name__ == '__main__':
+    main()
