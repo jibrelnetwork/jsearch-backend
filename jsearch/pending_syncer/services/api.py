7367d56eaacf04657c72cf9ce8efd577c2858f02
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-12-05 15:16:24 +0300

    ETHBE-719
    Add graceful shutdown to db_pool_raw

--- jsearch/pending_syncer/services/api.py
+++ jsearch/pending_syncer/services/api.py
@@ -60,5 +60,7 @@ async def on_startup(app: web.Application) -> None:
 
 async def on_shutdown(app: web.Application) -> None:
     app['db_pool'].close()
+    app['db_pool_raw'].close()
+
     await app['db_pool'].wait_closed()
-    await app['db_pool_raw'].close()
+    await app['db_pool_raw'].wait_closed()
