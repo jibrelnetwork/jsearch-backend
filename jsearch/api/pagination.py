977c41ace2c716dcdd07315dae0f95a5d7e2f4f2
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-12-06 15:02:28 +0300

    ETHBE-743
    Extend pagination sections with new next and link kwargs sections

--- jsearch/api/pagination.py
+++ jsearch/api/pagination.py
@@ -1,4 +1,5 @@
 import decimal
+from copy import copy
 
 from typing import NamedTuple, List, Dict, Any, Optional
 from yarl import URL
@@ -6,28 +7,51 @@ from yarl import URL
 from jsearch.api.ordering import Ordering
 
 
-class Page(NamedTuple):
-    link: Optional[str]
-    next_link: Optional[str]
+class Link(NamedTuple):
+    url: URL
+    kwargs: Dict[str, Any]
+
+    def __str__(self) -> str:
+        absolute_url = self.url.with_query(**self.kwargs)
+        return str(absolute_url)
+
+
+class PaginationBlock(NamedTuple):
+    link: Optional[Link]
+    next_link: Optional[Link]
 
     items: List[Any]
 
     def to_dict(self) -> Dict[str, Any]:
-        return {
-            "next": self.next_link,
-            "link": self.link
+        block = {
+            "next": None,
+            "link": None,
+            "next_kwargs": None,
+            "link_kwargs": None
         }
 
+        if self.next_link:
+            block.update({
+                "next": str(self.next_link),
+                "next_kwargs": self.next_link.kwargs,
+            })
+        if self.link:
+            block.update({
+                "link": str(self.link),
+                "link_kwargs": self.link.kwargs
+            })
 
-def get_link(
-        url: URL,
+        return block
+
+
+def make_link_query(
         fields: List[str],
         item: Dict[str, Any],
         params: Dict[str, Any],
         decimals_to_ints: bool,
         mapping: Optional[Dict[str, str]] = None,
-) -> Optional[str]:
-    query = dict()
+) -> Dict[str, Any]:
+    query = {}
 
     for field in fields:
         query_key = mapping and mapping.get(field) or field
@@ -39,14 +63,11 @@ def get_link(
 
         query[query_key] = str(value)
 
-    absolute_url = url.with_query({**query, **params})
-    if absolute_url:
-        return str(absolute_url)
-
-    return None
+    query.update(params)
+    return query
 
 
-def get_page(
+def get_pagination_description(
         url: URL,
         limit: int,
         ordering: Ordering,
@@ -55,7 +76,7 @@ def get_page(
         mapping: Optional[Dict[str, str]] = None,
         url_params: Optional[Dict[str, Any]] = None,
         decimals_to_ints: bool = False,
-) -> Page:
+) -> PaginationBlock:
     """
     If there we have (limit + 1) items - we can
     create link to next chunk. Next chunk starts
@@ -68,31 +89,30 @@ def get_page(
     if url_params:
         params.update(url_params)
 
+    # based on last element - cursor link to next chunk
+    next_link = None
     if len(items) > limit:
         next_chunk_item = items[-1]
-        next_link: Optional[str] = get_link(
-            url,
+        next_link_kwargs = make_link_query(
             key_set_fields or ordering.fields,
             next_chunk_item,
             mapping=mapping,
             params=params,
             decimals_to_ints=decimals_to_ints,
         )
-
+        next_link = Link(url, next_link_kwargs)
         items = items[:-1]
-    else:
-        next_link = None
 
+    # cursor on current chunk - based on first element
+    link = None
     if items:
-        link: Optional[str] = get_link(
-            url,
+        link_kwargs = make_link_query(
             key_set_fields or ordering.fields,
             items[0],
             mapping=mapping,
             params=params,
             decimals_to_ints=decimals_to_ints,
         )
-    else:
-        link = None
+        link = Link(url, link_kwargs)
 
-    return Page(link=link, next_link=next_link, items=items)
+    return PaginationBlock(link=link, next_link=next_link, items=items)
