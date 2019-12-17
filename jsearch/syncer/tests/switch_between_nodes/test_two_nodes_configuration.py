4cfaece58ee679f386e3e52463396aff81697eb6
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2019-12-17 14:33:57 +0300

    ETHBE-752
    A better naming for syncer switch tests.

--- jsearch/syncer/tests/switch_between_nodes/test_two_nodes_configuration.py
+++ jsearch/syncer/tests/switch_between_nodes/test_two_nodes_configuration.py
@@ -13,27 +13,24 @@ pytestmark = pytest.mark.asyncio
 
 @dataclass
 class NodeDescription:
+    id: str
     blocks: int = 0
-    from_number: int = 0
-    from_hash: Optional[str] = None
+
+    start_number: int = 0
+    start_hash: Optional[Union[str, ChainPointer]] = None
 
 
 @dataclass
 class SwitchCase:
     label: str
 
+    left: NodeDescription
+    right: NodeDescription
+
     blocks: Optional[BlockRange] = None
     forked: Optional[BlockRange] = None
-    left_blocks: int = 0
-    left_start_number: int = 0
-
-    right_blocks: int = 0
-    right_start_number: int = 0
 
     synced_range: Optional[BlockRange] = None
-
-    left_start_hash: Optional[Union[str, ChainPointer]] = None
-    right_start_hash: Optional[Union[str, ChainPointer]] = None
 
     left_id: str = '0xleft'
     right_id: str = '0xright'
@@ -42,10 +39,10 @@ class SwitchCase:
     exception: Exception = None
 
     def get_right_start_hash(self, left: Chain):
-        if isinstance(self.right_start_hash, ChainPointer):
-            return self.right_start_hash.get_hash(left)
-        if self.right_start_hash:
-            return self.right_start_hash
+        if isinstance(self.right.start_hash, ChainPointer):
+            return self.right.start_hash.get_hash(left)
+        if self.right.start_hash:
+            return self.right.start_hash
         return left.last.hash
 
     def __str__(self):
@@ -61,40 +58,30 @@ cases = [
     SwitchCase(
         label='right node is in canonical chain',
         blocks=BlockRange(1, 20),
-        left_blocks=10,
-        left_start_number=1,
-        right_blocks=10,
-        right_start_number=11,
+        left=NodeDescription(id='0xleft', blocks=10, start_number=1),
+        right=NodeDescription(id='0xright', blocks=10, start_number=11),
         synced_range=BlockRange(1, 20)
     ),
     SwitchCase(
         label='right node is in fork chain',
         blocks=BlockRange(1, 20),
         forked=BlockRange(6, 10),
-        left_blocks=10,
-        left_start_number=1,
-        right_blocks=15,
-        right_start_number=6,
-        right_start_hash=ChainPointer(offset=5),
+        left=NodeDescription(id='0xleft', blocks=10, start_number=1),
+        right=NodeDescription(id='0xright', blocks=15, start_number=6, start_hash=ChainPointer(offset=6)),
         synced_range=BlockRange(1, 20)
     ),
     SwitchCase(
         label='right has no events',
         blocks=BlockRange(1, 20),
-        left_blocks=10,
-        left_start_number=1,
-        right_blocks=0,
-        right_start_number=10,
+        left=NodeDescription(id='0xleft', blocks=10, start_number=1),
+        right=NodeDescription(id='0xright', blocks=0, start_number=10),
         synced_range=BlockRange(1, 20)
     ),
     SwitchCase(
         label='there is not common blocks',
         blocks=BlockRange(1, 20),
-        left_blocks=10,
-        left_start_number=1,
-        right_blocks=10,
-        right_start_number=10,
-        right_start_hash='0xfork',
+        left=NodeDescription(id='0xleft', blocks=10, start_number=1),
+        right=NodeDescription(id='0xfork', blocks=10, start_number=10, start_hash='0xfork'),
         should_switch=False,
         synced_range=BlockRange(1, 20),
         exception=ValueError(f'No common block between 0xleft and 0xright')
@@ -102,10 +89,8 @@ cases = [
     SwitchCase(
         label='a gap between nodes',
         blocks=BlockRange(1, 10),
-        left_blocks=10,
-        left_start_number=1,
-        right_blocks=10,
-        right_start_number=12,
+        left=NodeDescription(id='0xleft', blocks=10, start_number=1),
+        right=NodeDescription(id='0xright', blocks=10, start_number=12),
         should_switch=False,
         synced_range=BlockRange(1, 20),
     ),
@@ -121,15 +106,15 @@ def generate_chains(chain_generator: ChainGenerator, given: SwitchCase, raw_db: 
     right = chain_generator(node_id=given.right_id)
 
     left.create_chain(
-        given.left_blocks,
-        start_from=given.left_start_number,
-        start_from_hash=given.left_start_hash
+        given.left.blocks,
+        start_from=given.left.start_number,
+        start_from_hash=given.left.start_hash
     )
     left.collector.flush(raw_db)
 
     right.create_chain(
-        given.right_blocks,
-        start_from=given.right_start_number,
+        given.right.blocks,
+        start_from=given.right.start_number,
         start_from_hash=given.get_right_start_hash(left.chain)
     )
     right.collector.flush(raw_db)
@@ -152,7 +137,7 @@ def catch_on_condition(exception: Optional[Exception]):
     cases,
     ids=[*map(str, cases)]
 )
-async def test_switch_right_node_is_in_canonical_chain(
+async def test_switch_events_chain(
         db: Engine,
         raw_db,
         sync: Callable[[BlockRange, str], None],
@@ -190,6 +175,6 @@ async def test_switch_right_node_is_in_canonical_chain(
         f'select count(block_hash) as blocks, node_id from chain_events group by node_id;'
     ).fetchall()
     results = {item['node_id']: item['blocks'] for item in results}
-    assert results.get(given.left_id, 0) == given.left_blocks
+    assert results.get(given.left_id, 0) == given.left.blocks
     if given.should_switch:
-        assert results.get(given.right_id, 0) == given.right_blocks
+        assert results.get(given.right_id, 0) == given.right.blocks
