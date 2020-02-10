5eed37dccbbf1e0f1164fd1a12e601b2bfcaa8b8
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2020-02-10 19:29:25 +0300

    Provide a proxy endpoint to send raw transaction via geth-forks

--- jsearch/api/node_proxy.py
+++ jsearch/api/node_proxy.py
@@ -1,3 +1,4 @@
+import asyncio
 from jsearch import settings
 from typing import Any, List
 
@@ -20,40 +21,54 @@ def _decimal_result(res):
 class NodeProxy:
 
     def __init__(self, node_url):
-        self.node_url = node_url
+        if isinstance(node_url, str):
+            self.node_urls = [node_url, ]
+        elif isinstance(node_url, list):
+            self.node_urls = node_url
+
+    async def _fetch(self, session, url, message, headers):
+        async with session.post(url, json=message, headers=headers) as resp:
+            return await resp.json()
 
     @backoff.on_exception(backoff.expo, aiohttp.ClientError, max_tries=MAX_RETRY)
     async def _request(self, method: str, params: List = None):
         params = params or []
         message = {'jsonrpc': '2.0', 'method': method, 'params': params, 'id': 1}
         headers = {'User-Agent': settings.HTTP_USER_AGENT}
+
         async with aiohttp.ClientSession() as session:
-            async with session.post(self.node_url, json=message, headers=headers) as resp:
-                return await resp.json()
+            tasks = [self._fetch(session, url, message, headers) for url in self.node_urls]
+
+            return await asyncio.gather(*tasks)
 
     async def client_version(self) -> str:
         res = await self._request('web3_clientVersion')
-        return res
+        # we need to get a response from the first node only
+        return res[0]
 
     async def gas_price(self) -> str:
         res = await self._request('eth_gasPrice')
-        res = _decimal_result(res)
+        # we need to get a response from the first node only
+        res = _decimal_result(res[0])
         return res
 
     async def call_contract(self, args: List) -> Any:
         res = await self._request('eth_call', args)
-        return res
+        # we need to get a response from the first node only
+        return res[0]
 
     async def estimate_gas(self, args: List) -> str:
         res = await self._request('eth_estimateGas', args)
-        res = _decimal_result(res)
+        # we need to get a response from the first node only
+        res = _decimal_result(res[0])
         return res
 
     async def send_raw_transaction(self, args: List) -> str:
         res = await self._request('eth_sendRawTransaction', args)
-        return res
+        return res[0]  # ToDo: check responses from all urls
 
     async def transaction_count(self, args: List) -> str:
         res = await self._request('eth_getTransactionCount', args)
-        res = _decimal_result(res)
+        # we need to get a response from the first node only
+        res = _decimal_result(res[0])
         return res
