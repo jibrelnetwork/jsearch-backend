5a6fc1048202c1bf0740c8224a55b4c4c533c4ab
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2020-02-18 16:11:34 +0300

    Add missing imports from utils

--- jsearch/common/contracts/__init__.py
+++ jsearch/common/contracts/__init__.py
@@ -1,5 +1,6 @@
 from jsearch.typing import AccountAddress
 
 from .erc20 import ERC20_ABI, ERC20_ABI_SIMPLE, ERC20_DEFAULT_DECIMALS, ERC20_METHODS_IDS, Abi_ERC20  # noqa: F401
+from .utils import decode_event
 
 NULL_ADDRESS: AccountAddress = '0x0000000000000000000000000000000000000000'
