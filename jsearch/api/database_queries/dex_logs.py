ea180f570b01126ee4d4ad9367c9517354984ab0
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2020-02-24 18:24:19 +0300

    ETHBE-798
    Adds new endpoints which are related with dex events.
    
    /v1/dex/history/<token_address>
    /v1/dex/orders/<token_address>

---
+++ jsearch/api/database_queries/dex_logs.py
@@ -0,0 +1,154 @@
+from _operator import and_
+from functools import reduce
+from typing import List, Optional
+
+from sqlalchemy import Column, select, false
+from sqlalchemy.sql import ClauseElement
+
+from jsearch.api.ordering import Ordering, ORDER_SCHEME_BY_NUMBER, ORDER_SCHEME_BY_TIMESTAMP, get_ordering
+from jsearch.common.processing.dex_logs import DexEventType
+from jsearch.common.tables import dex_logs_t
+from jsearch.typing import OrderScheme, OrderDirection, Columns
+
+
+def get_default_fields() -> List[Column]:
+    return [
+        dex_logs_t.c.block_hash,
+        dex_logs_t.c.block_number,
+        dex_logs_t.c.timestamp,
+        dex_logs_t.c.tx_hash,
+        dex_logs_t.c.event_type,
+        dex_logs_t.c.event_data,
+    ]
+
+
+def get_events_ordering(scheme: OrderScheme, direction: OrderDirection) -> Ordering:
+    columns: Columns = {
+        ORDER_SCHEME_BY_NUMBER: [dex_logs_t.c.block_number],
+        ORDER_SCHEME_BY_TIMESTAMP: [dex_logs_t.c.timestamp],
+    }[scheme]
+
+    return get_ordering(columns, scheme, direction)
+
+
+def get_clause(key, value) -> ClauseElement:
+    filtered_key = dex_logs_t.c.event_data[key].astext
+    if isinstance(value, (list, tuple)):
+        q = filtered_key.in_(map(str, value))
+    else:
+        q = filtered_key == value
+    return q
+
+
+def get_dex_logs_query(
+        event_types: List[str],
+        **data_filter
+) -> ClauseElement:
+    assert event_types
+
+    columns = get_default_fields()
+
+    query = select(columns).where(
+        and_(
+            dex_logs_t.c.event_type.in_(event_types),
+            dex_logs_t.c.is_forked == false()
+        )
+    )
+
+    from sqlalchemy.dialects.postgresql import dialect
+    filters = [get_clause(key, value) for key, value in data_filter.items()]
+    if filters:
+        query = query.where(reduce(and_, filters[1:], filters[0]))
+        print(query.compile(dialect=dialect(), compile_kwargs={"literal_binds": True}))
+    return query
+
+
+def get_dex_events_query(
+        orders_ids: List[int],
+        trades_ids: List[str],
+        ordering: Ordering,
+        limit: Optional[int] = None,
+        events_types: Optional[List[str]] = None,
+        block_number: Optional[int] = None,
+        timestamp: Optional[int] = None,
+) -> ClauseElement:
+    if not events_types:
+        events_types = [
+            DexEventType.ORDER_PLACED,
+            DexEventType.ORDER_ACTIVATED,
+            DexEventType.ORDER_COMPLETED,
+            DexEventType.ORDER_CANCELLED,
+            DexEventType.ORDER_EXPIRED,
+            DexEventType.TRADE_PLACED,
+            DexEventType.TRADE_COMPLETED,
+            DexEventType.TRADE_CANCELLED,
+        ]
+
+    query = get_dex_logs_query(event_types=events_types)
+    if block_number:
+        query = query.where(ordering.operator_or_equal(dex_logs_t.c.block_number, block_number))
+
+    if timestamp:
+        query = query.where(ordering.operator_or_equal(dex_logs_t.c.block_number, block_number))
+
+    order_ids_q = get_clause('orderID', orders_ids)
+    trade_ids_q = get_clause('tradeID', trades_ids)
+
+    query = query.where(order_ids_q | trade_ids_q)
+    query = query.order_by(*ordering.columns)
+
+    if limit:
+        query = query.limit(limit)
+
+    return query
+
+
+def get_dex_orders_query(
+        traded_asset: Optional[str] = None,
+        creator: Optional[str] = None,
+) -> ClauseElement:
+    filter_kwargs = {}
+
+    if creator:
+        filter_kwargs['orderCreator'] = traded_asset
+
+    if traded_asset:
+        filter_kwargs['tradedAsset'] = traded_asset
+
+    return get_dex_logs_query(
+        event_types=[
+            DexEventType.ORDER_PLACED
+        ],
+        **filter_kwargs
+    )
+
+
+def get_dex_orders_events_query(ids: List[int]) -> ClauseElement:
+    return get_dex_logs_query(
+        event_types=[
+            DexEventType.ORDER_ACTIVATED,
+            DexEventType.ORDER_COMPLETED,
+            DexEventType.ORDER_CANCELLED,
+            DexEventType.ORDER_EXPIRED
+        ],
+        orderID=ids
+    )
+
+
+def get_dex_trades_query(order_ids: List[int]) -> ClauseElement:
+    return get_dex_logs_query(
+        event_types=[
+            DexEventType.TRADE_PLACED,
+        ],
+        orderID=order_ids
+    )
+
+
+def get_dex_trades_events_query(trade_ids: List[int]) -> ClauseElement:
+    return get_dex_logs_query(
+        event_types=[
+            DexEventType.TRADE_COMPLETED,
+            DexEventType.TRADE_CANCELLED,
+        ],
+        tradeID=trade_ids
+    )
---
+++ jsearch/api/handlers/dex.py
@@ -0,0 +1,81 @@
+import logging
+from typing import Optional, List
+
+from aiohttp import web
+
+from jsearch.api.blockchain_tip import maybe_apply_tip
+from jsearch.api.helpers import ApiError, api_success, maybe_orphan_request
+from jsearch.api.ordering import Ordering
+from jsearch.api.pagination import get_pagination_description
+from jsearch.api.serializers.dex import DexHistorySchema, DexOrdersSchema
+from jsearch.api.utils import use_kwargs
+
+logger = logging.getLogger(__name__)
+
+PENDING_EVENTS_DEFAULT_LIMIT = 100
+
+
+@ApiError.catch
+@use_kwargs(DexHistorySchema)
+async def get_dex_history(
+        request: web.Request,
+        order: Ordering,
+        limit: int,
+        token_address: str,
+        tip_hash: Optional[str] = None,
+        event_type: Optional[List[str]] = None,
+        block_number: Optional[int] = None,
+        timestamp: Optional[int] = None,
+) -> web.Response:
+    storage = request.app['storage']
+    last_known_chain_event_id = await storage.get_latest_chain_event_id()
+
+    dex_history, last_affected_block = await storage.get_dex_history(
+        ordering=order,
+        limit=limit,
+        token_address=token_address,
+        event_type=event_type,
+        block_number=block_number,
+        timestamp=timestamp
+    )
+    url = request.app.router['dex_orders'].url_for(token_address=token_address)
+    page = get_pagination_description(url=url, items=dex_history, limit=limit, ordering=order)
+
+    data, tip = await maybe_apply_tip(storage, tip_hash, dex_history, last_affected_block, empty=[])
+    orphaned_request = await maybe_orphan_request(
+        request,
+        last_chain_event_id=last_known_chain_event_id,
+        last_data_block=last_affected_block,
+        last_tip_block=tip and tip.last_number,
+    )
+
+    if orphaned_request is not None:
+        return orphaned_request
+    return api_success(data=[item.as_dict() for item in dex_history], page=page)
+
+
+@ApiError.catch
+@use_kwargs(DexOrdersSchema)
+async def get_dex_orders(
+        request: web.Request,
+        token_address: str,
+        order_status: Optional[List[str]] = None,
+        order_creator: Optional[str] = None,
+        tip_hash: Optional[str] = None,
+) -> web.Response:
+    storage = request.app['storage']
+    last_known_chain_event_id = await storage.get_latest_chain_event_id()
+
+    dex_history, last_affected_block = await storage.get_dex_orders(token_address, order_status, order_creator)
+
+    data, tip = await maybe_apply_tip(storage, tip_hash, dex_history, last_affected_block, empty=[])
+    orphaned_request = await maybe_orphan_request(
+        request,
+        last_known_chain_event_id,
+        last_affected_block,
+        tip and tip.last_number,
+    )
+
+    if orphaned_request is not None:
+        return orphaned_request
+    return api_success(data=[item.as_dict() for item in dex_history])
