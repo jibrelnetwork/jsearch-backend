cb535bfdbe6b3d1e56d0037158cd939b6c39cade
Aleksey Selikhov <aleksey.selikhov@gmail.com>
2020-02-25 13:33:32 +0300

    Add a new parameter uncle_index to get an uncle by block number/hash and Index like json rpc methods eth_getUncleByBlockHashAndIndex and eth_getUncleByBlockNumberAndIndex

--- jsearch/api/handlers/blocks.py
+++ jsearch/api/handlers/blocks.py
@@ -14,6 +14,7 @@ from jsearch.api.helpers import (
 from jsearch.api.ordering import Ordering
 from jsearch.api.pagination import get_pagination_description
 from jsearch.api.serializers.blocks import BlockListSchema, BlockTransactionsIndexSchema
+from jsearch.api.serializers.uncles import BlockUnclesIndexSchema
 from jsearch.api.utils import use_kwargs
 
 
@@ -94,10 +95,12 @@ async def get_block_transaction_count(request):
     return api_success({"count": len(block['transactions'])})
 
 
-async def get_block_uncles(request):
+@ApiError.catch
+@use_kwargs(BlockUnclesIndexSchema())
+async def get_block_uncles(request, uncle_index: Optional[int] = None):
     storage = request.app['storage']
     tag = get_tag(request)
-    uncles = await storage.get_block_uncles(tag)
+    uncles = await storage.get_block_uncles(tag, uncle_index)
     return api_success([u.to_dict() for u in uncles])
 
 
