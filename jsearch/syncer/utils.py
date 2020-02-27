cb16b526106d7e2b3e8006bf88c1d8da6d4738f3
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2020-02-27 15:53:18 +0300

    Add a new syncer feature - waiting migration applying

--- jsearch/syncer/utils.py
+++ jsearch/syncer/utils.py
@@ -1,17 +1,38 @@
+import logging
+import time
+from typing import Optional
+
 from sqlalchemy import create_engine
 
 from jsearch import settings
+
+logger = logging.getLogger(__name__)
+
+
+def execute(connection_string, query: str, *params):
+    engine = create_engine(connection_string)
+
+    cursor = engine.execute(query, *params)
+    return cursor.fetchone()
+
+
+def wait_for_version(version_id: str, timeout: float = 5.0) -> None:
+    query = "SELECT is_applied FROM goose_db_version WHERE version_id = %s;"
+
+    status = False
+    while not status:
+        logger.info("Waiting migration", extra={'version_id': version_id, 'timeout': 5})
+        row = execute(settings.JSEARCH_MAIN_DB, query, version_id)
+        status = row and row['is_applied']
+        if not status:
+            time.sleep(timeout)
 
 
 def get_last_block() -> int:
     query = """
     SELECT block_number FROM headers ORDER BY block_number DESC LIMIT 1;
     """
-
-    engine = create_engine(settings.JSEARCH_RAW_DB)
-    cursor = engine.execute(query)
-    row = cursor.fetchone()
-
+    row = execute(settings.JSEARCH_RAW_DB, query)
     if row:
         return row['block_number']
     return 0
