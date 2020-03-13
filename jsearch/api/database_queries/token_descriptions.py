1f3148d7990630243b01e177f5d33d2d50d9ab77
Andrew Demyanenko <andrew.demyanenko@jibrel.network>
2020-03-13 13:57:12 +0300

    Fix queries for token holders

--- jsearch/api/database_queries/token_descriptions.py
+++ jsearch/api/database_queries/token_descriptions.py
@@ -7,7 +7,7 @@ from jsearch.typing import TokenAddress
 
 def get_default_fields():
     return [
-        token_descriptions_t.c.token,
+        token_descriptions_t.c.address,
         token_descriptions_t.c.total_supply,
         token_descriptions_t.c.block_number,
         token_descriptions_t.c.block_hash
@@ -18,7 +18,7 @@ def get_token_threshold_query(token_address: TokenAddress) -> ClauseElement:
     columns = get_default_fields()
     return select(columns).where(
         and_(
-            token_descriptions_t.c.token_address == token_address,
+            token_descriptions_t.c.address == token_address,
             token_descriptions_t.c.is_forked == false(),
         )
     ).order_by(desc(token_descriptions_t.c.block_number)).limit(1)
